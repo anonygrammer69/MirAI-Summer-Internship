e683e928d1ea9d4e2d515fb5b789b2443391e93b
darealLord <133997594+anonygrammer69@users.noreply.github.com>
2026-07-10 21:41:38 +0530

    changes

--- 2nd Assignment/app2.py
+++ 2nd Assignment/app2.py
@@ -13,23 +13,6 @@ from dotenv import load_dotenv
 load_dotenv()
 client=genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
-user_message=st.text_input("Say something: ")
-if st.button("SEND"):
-    if user_message:
-        ai_instructions= f"You are acting as {personality}.Respond to the message sent by the user staying completely in character: {user_message} "
-        
-        with st.spinner("Connecting to the multiverse!......"):
-            response=client.models.generate_content(
-                model="gemini-2.5-flash",
-                contents=ai_instructions
-            )
-                
-
-            st.success("Message received!")
-            st.write(response.text)
-    
-    else:
-        st.warning("Please type a message first")
 
 if "chat_histories" not in st.session_state:
     st.session_state.chat_histories = {}
